51b36d94391351f2360374713cf012176acaf4c9
Daniel Esteban Cardona Rojas <d.cardona.rojas@gmail.com>
2021-03-27 13:34:21 -0500

    Fix SwiftPM support

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Auk",
+    platforms: [
+        .iOS(.v13)
+        
+    ],
     products: [
         .library(name: "Auk", targets: ["Auk"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/evgenyneu/moa", from: "12.0.0")
     ],
     targets: [
-        .target(name: "Auk", dependencies: [], path: "Auk"),
+        .target(name: "Auk", dependencies: ["moa"], path: "Auk"),
         .testTarget(
             name: "AukTests",
             dependencies: ["Auk"],
